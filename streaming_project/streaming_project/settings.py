162217bc325038300a15c767bbb89e7bd56cca9f
rodrigo <rozanon@yahoo.com.br>
2023-09-04 15:28:11 -0300

    AWS ajuste7

--- streaming_project/streaming_project/settings.py
+++ streaming_project/streaming_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=3(2@)76%6#2l7qw)p(&mfs&y9&v8le8$f$of6nv7v_j_vafyq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.173.207.21', '54.196.169.156']
+ALLOWED_HOSTS = ['54.173.207.21', '54.196.169.156', '127.0.0.1']
 
 # Application definition
 
